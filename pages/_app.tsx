8b217aa72f933b54a26b340a125ba41c38e1217e
Eunbi Kang <jenabill@naver.com>
2022-08-20 13:14:35 +0900

    refactor: _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material';
+import Layout from '../components/Layout';
+import { theme } from '../styles/theme';
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </RecoilRoot>
+  );
 }
 
-export default MyApp
+export default MyApp;
