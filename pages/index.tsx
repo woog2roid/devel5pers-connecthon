85622f4b47b9fb6a94c2a65eb647b39cf6c86edc
HyezNee <46377695+HyezNee@users.noreply.github.com>
2022-08-20 15:09:07 +0900

    add MenuBtnList component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import LoginForm from '../components/login/LoginForm';
 import sessionState from '../store/session';
 import { getSession } from '../apis/auth';
 import { supabase } from '../utils/supabase';
+import Events from '../components/Events';
+import MenuBtnList from '../components/MenuBtnList';
 
 const Home: NextPage = () => {
   const [session, setSession] = useRecoilState(sessionState);
@@ -16,7 +18,18 @@ const Home: NextPage = () => {
       setSession(session);
     });
   }, [setSession]);
-  return <>{!session ? <LoginForm /> : 'Hello, World!'}</>;
+  return (
+    <>
+      {
+        !session ? 
+          <LoginForm /> :
+          <>
+          <Events />
+          <MenuBtnList />
+          </>
+      }
+    </>
+  );
 };
 
-export default Home;
+export default Home
