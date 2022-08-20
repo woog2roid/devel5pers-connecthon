fff333e8d0e3114f57e04309b8fb00e3c337897a
Eunbi Kang <jenabill@naver.com>
2022-08-21 00:50:57 +0900

    refactor and design: TopBar title에 아이콘 추가

--- components/common/TopBar/style.tsx
+++ components/common/TopBar/style.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import { MenuItem, Menu, ListItem } from '@mui/material';
+import { COLORS } from '../../../styles/palette';
 
 export const Wrapper = styled.div`
   padding: 3px;
@@ -9,6 +10,12 @@ export const Wrapper = styled.div`
 
   .title {
     font-size: 18px;
+    display: flex;
+    align-items: center;
+    gap: 0.2rem;
+    svg {
+      color: ${COLORS.green};
+    }
   }
 `;
 
