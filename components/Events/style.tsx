0f18999f0c3a724696f73b73ce42913463d9b860
HyezNee <46377695+HyezNee@users.noreply.github.com>
2022-08-20 13:28:02 +0900

    Feat: Add Events, Topbar component

---
+++ components/Events/style.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Image = styled.div`
+    display: flex;
+    height:300px;
+    background-repeat: no-repeat;
+    overflow: hidden;
+    justify-content: center;
+`;
