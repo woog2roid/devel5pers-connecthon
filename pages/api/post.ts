eb67f8c72437992f0d50c29fa2fd996917ca79f0
woog2roid <wooguijung@korea.ac.kr>
2022-08-20 11:27:12 +0900

    feat(api): post API 수정

--- pages/api/post.ts
+++ pages/api/post.ts
@@ -2,35 +2,27 @@ import { supabase } from '../../utils/supabase';
 import { definitions } from '../../types/supabase';
 
 export const getPostById = async (id: number) => {
-  return supabase
-    .from<definitions['posts']>('posts')
-    .select()
-    .match({
-      id,
-    });
+  return supabase.from<definitions['posts']>('posts').select().match({
+    id,
+  });
 };
 
-// 모든 포스트를 받아옴
-/*
-    후에 follow/following 기능이 생기면서 수정될 예정
-*/
-export const getPosts = async () => {
-  return supabase
-    .from<definitions['posts']>('posts')
-    .select()
-    .order('created_at');
+export const getPostsByCategory = async (category: string) => {
+  return supabase.from<definitions['posts']>('posts').select().match({
+    category,
+  });
 };
 
 export const addPost = async (
   userId: string,
-  certificate: string,
+  category: string,
   description: string,
-  image: string,
+  image: string[],
 ) => {
   return supabase.from<definitions['posts']>('posts').insert([
     {
       author_id: userId,
-      certificate,
+      category,
       description,
       image,
     },
