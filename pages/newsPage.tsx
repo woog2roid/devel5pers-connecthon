3bb35d2e866849db67b1d88bad466f8fa6b33ea4
Eunbi Kang <jenabill@naver.com>
2022-08-21 01:53:10 +0900

    refactor: news page에 CustomHead, TopHeading 컴포넌트 추가

--- pages/newsPage.tsx
+++ pages/newsPage.tsx
@@ -1,10 +1,18 @@
+import CustomHead from '../components/common/CustomHead';
+import TopHeading from '../components/common/TopHeading';
 import NewsList from '../components/news/NewsList';
 
 export default function NewsPage() {
-    return (
-        <div style={{textAlign: "center", margin : "10px", marginBottom: "20px"}}>
-            <p style={{marginBottom: "10px", fontSize: "12px"}}>📰 매일 새로 업데이트 되는 환경 관련 뉴스를 읽어보세요.</p>
-            <NewsList />
-        </div>
-    );
+  return (
+    <>
+      <CustomHead title="뉴스 | AimEco" />
+      <div style={{ textAlign: 'center', padding: '1rem 0' }}>
+        <TopHeading title="뉴스" to="/" />
+        <p style={{ margin: '10px 0', fontSize: '12px' }}>
+          📰 매일 새로 업데이트 되는 환경 관련 뉴스를 읽어보세요.
+        </p>
+        <NewsList />
+      </div>
+    </>
+  );
 }
