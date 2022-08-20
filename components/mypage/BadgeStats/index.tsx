4e27682845620696c160c4bc28b63fa2cc2ea0ca
Eunbi Kang <jenabill@naver.com>
2022-08-20 22:55:57 +0900

    refactor: getBadges 함수 로직 수정

--- components/mypage/BadgeStats/index.tsx
+++ components/mypage/BadgeStats/index.tsx
@@ -5,9 +5,10 @@ import BadgeItem from '../BadgeItem';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { badgeListState } from '../../../store/badge';
 import userState from '../../../store/user';
+import { getUser } from '../../../apis/auth';
 
 const BadgeStats = () => {
-  const user = useRecoilValue(userState);
+  const [user, setUser] = useRecoilState(userState);
   const [badges, setBadges] = useRecoilState(badgeListState);
 
   useEffect(() => {
@@ -16,29 +17,32 @@ const BadgeStats = () => {
       if (user !== null) {
         const result = await getBadgesByUserId(user?.id);
         setBadges(result);
+      } else {
+        const data = getUser();
+        setUser(data);
       }
     };
     getBadges();
   }, [user]);
 
-  return badges !== null ? (
+  return (
     <Wrapper>
       <div>
         <p>⭐ 뱃지 개수</p>
-        <p className="large">{badges.length}개</p>
+        <p className="large">{badges === null ? null : `${badges.length}개`}</p>
       </div>
       <div>
         <p>🏆 최근 획득한 뱃지</p>
         <div>
-          {badges.length > 0 ? (
-            <BadgeItem badge={badges[0].badges} cursor={false} />
+          {badges === null ? null : badges.length > 0 ? (
+            <BadgeItem badge={badges[0]} cursor={false} main={false} />
           ) : (
             <></>
           )}
         </div>
       </div>
     </Wrapper>
-  ) : null;
+  );
 };
 
 export default BadgeStats;
