eb0bad256243470cc5d891725b9b132f4137919e
woog2roid <wooguijung@korea.ac.kr>
2022-08-20 15:35:46 +0900

    setup(storybook): localhost 이미지 도메인 추가

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['http://localhost:3000'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
