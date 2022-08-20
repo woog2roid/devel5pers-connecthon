ba23fe49e175fd26e0ac3545b209a1570885321d
BeLeap <beleap@beleap.codes>
2022-08-21 01:40:02 +0900

    feat:update username

--- pages/mypage/edit/profile.tsx
+++ pages/mypage/edit/profile.tsx
@@ -1,5 +1,79 @@
+import styled from "@emotion/styled";
+import Image from "next/image"
+import { useState } from "react";
+import { useRecoilValue } from "recoil";
+import { getProfile, updateProfile } from "../../../apis/profile";
+import userState from "../../../store/user";
+import { definitions } from "../../../types/supabase";
+import { useEffect } from "react";
+
 const ProfileEdit = () => {
-  return <>Profile Edit</>;
+  const user = useRecoilValue(userState)
+  const [profile, setProfile] = useState<definitions['profiles']>()
+
+  useEffect(() => {
+      (async () => {
+        setProfile((await getProfile(user?.id ?? ''))[0])
+      })();
+  }, [setProfile])
+
+  const onDone = async () => {
+    if (user !== null && profile !== undefined) {
+      await updateProfile(profile, user?.id)
+    }
+  }
+
+  return (
+    <Wrapper>
+      { user ?
+        <>
+          <Image
+            src={user.user_metadata['avatar_url']}
+            alt="user profile image"
+            width="200"
+            height="200"
+          />
+          <NameInput value={profile?.name} onChange={(e) => {
+            if (profile !== undefined) {
+              const newProfile = {
+                ...profile,
+                name: e.target.value,
+              }
+              setProfile(newProfile)
+            }
+          }} />
+          <button onClick={onDone} style={{ 
+            margin: '5px',
+            backgroundColor: 'lime',
+          }}>DummyOnDone</button>
+        </> :
+        "Please Log in"
+      }
+    </Wrapper>
+  );
 };
 
 export default ProfileEdit;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+  padding: 5rem;
+`
+
+const NameInput = styled.input`
+  width: 15rem;
+  border-bottom: 1px solid grey;
+  border-top-style: hidden;
+  border-left-style: hidden;
+  border-right-style: hidden;
+  margin-top: 10rem;
+
+  &:focus {
+    outline: none;
+  }
+`
