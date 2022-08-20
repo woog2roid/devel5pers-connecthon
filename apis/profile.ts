1f5cbc870e8ee1eaf09a4a4c1e488591fb0e77e3
BeLeap <beleap@beleap.codes>
2022-08-20 13:34:33 +0900

    feat:unset representative badge

--- apis/profile.ts
+++ apis/profile.ts
@@ -12,3 +12,15 @@ export const setRepresentativeBadge = async (userId: string, badgeId: number) =>
       badge_id: badgeId,
     });
 }
+
+export const unsetRepresentativeBadge = async (userId: string, badgeId: number) => {
+  await supabase
+    .from<definitions['profile_badge_mappings']>('profile_badge_mappings')
+    .update({
+      is_representative: false,
+    })
+    .match({
+      profile_id: userId,
+      badge_id: badgeId,
+    });
+}
