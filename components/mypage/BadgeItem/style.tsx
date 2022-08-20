9e3e0a1ce95a426908bd6a1482239b0927abd01e
Eunbi Kang <jenabill@naver.com>
2022-08-20 19:25:42 +0900

    design: BadgeItem component position 수정

--- components/mypage/BadgeItem/style.tsx
+++ components/mypage/BadgeItem/style.tsx
@@ -1,10 +1,10 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
+  position: relative;
   width: calc(100px + 6px);
   text-align: center;
   padding: 3px;
-
   .title {
     font-size: 13px;
   }
