688de5ddf0bdb4b6631c7971233b1b34360c0e67
woog2roid <wooguijung@korea.ac.kr>
2022-08-20 19:12:46 +0900

    feat(NavButton): Icons 추가

--- components/home/MenuBtn/index.tsx
+++ components/home/MenuBtn/index.tsx
@@ -1,10 +1,13 @@
 import { Row, Wrapper } from './style';
+import { Newspaper, LocalMall, Campaign, MenuBook } from '@mui/icons-material';
 
 interface IProp {
   name: string;
 }
 
 export default function MenuBtn({ name }: IProp) {
+  const sx = {};
+
   return (
     <>
       <Row>
@@ -17,8 +20,23 @@ export default function MenuBtn({ name }: IProp) {
                 width: '70px',
                 height: '70px',
                 backgroundColor: '#7FB77E',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
               }}
-            />
+            >
+              {name === '뉴스' ? (
+                <Newspaper sx={{ color: 'white' }} />
+              ) : name === '도서관' ? (
+                <MenuBook sx={{ color: 'white' }} />
+              ) : name === '캠페인' ? (
+                <Campaign sx={{ color: 'white' }} />
+              ) : name === '스토어' ? (
+                <LocalMall sx={{ color: 'white' }} />
+              ) : (
+                <></>
+              )}
+            </div>
           </Wrapper>
         </div>
         <span style={{ fontFamily: 'NotoSans' }}>{name}</span>
