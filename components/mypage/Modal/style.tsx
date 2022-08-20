9c519bf01e19da236d236ad00631b26507919572
woog2roid <wooguijung@korea.ac.kr>
2022-08-21 01:57:39 +0900

    design: modal width 수정

--- components/mypage/Modal/style.tsx
+++ components/mypage/Modal/style.tsx
@@ -3,7 +3,7 @@ export const modalStyle = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 300,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
