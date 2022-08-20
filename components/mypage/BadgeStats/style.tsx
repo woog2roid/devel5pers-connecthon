bb5313e2da858be7b5bc527510bcb55841d62a86
woog2roid <wooguijung@korea.ac.kr>
2022-08-20 17:08:08 +0900

    refactor: style.tsx 분리

---
+++ components/mypage/BadgeStats/style.tsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  border-radius: 1rem;
+  border: 1px solid lightgray;
+  box-shadow: 1px 5px 10px rgba(0, 0, 0, 0.2);
+  & > div {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem 0;
+    p.large {
+      font-weight: bold;
+      font-size: 1.9rem;
+    }
+  }
+  & > div:last-child {
+    border-left: 1px solid lightgray;
+  }
+`;
