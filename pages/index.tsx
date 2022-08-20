af0f17032ab8be724647690afdab6e25a089cdad
HyezNee <46377695+HyezNee@users.noreply.github.com>
2022-08-20 15:03:21 +0900

    minor: recover from strange state

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,20 @@ import { useRecoilState } from 'recoil';
 import LoginForm from '../components/login/LoginForm';
 import sessionState from '../store/session';
 import { getSession } from '../apis/auth';
+import Events from '../components/Events';
 
 const Home: NextPage = () => {
+
   const [session, setSession] = useRecoilState(sessionState);
   useEffect(() => {
     setSession(getSession());
   }, [setSession]);
-  return <>{!session ? <LoginForm /> : 'Hello, World!'}</>;
+
+  return (<>{!session ? <LoginForm /> : 
+    <>
+    <Events />
+    </>
+  }</>);
 };
 
 export default Home;
