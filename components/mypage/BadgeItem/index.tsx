4d03b63f8c7ffe58cb18aa4f70c2ec398dc9e892
Eunbi Kang <jenabill@naver.com>
2022-08-20 19:26:31 +0900

    refactor: BadgeItem props 및 UI 수정

--- components/mypage/BadgeItem/index.tsx
+++ components/mypage/BadgeItem/index.tsx
@@ -1,24 +1,59 @@
 import Image from 'next/image';
 import { Wrapper } from './style';
-import IBadge from '../../../types/badge';
+import { useState } from 'react';
+import { AiOutlineCheckCircle } from 'react-icons/ai';
+import styled from '@emotion/styled';
+import { COLORS } from '../../../styles/palette';
+import { useRecoilValue } from 'recoil';
+import userState from '../../../store/user';
+import {
+  setRepresentativeBadge,
+  unsetRepresentativeBadge,
+} from '../../../apis/profile';
 
-const BadgeItem = ({ id, iconUrl, name }: IBadge) => {
+const BadgeItem = ({ badge, cursor, is_representative }: any) => {
+  const user = useRecoilValue(userState);
+  const { iconUrl, name } = badge;
+  const [toggle, setToggle] = useState<boolean>(is_representative);
+  const onToggle = () => {
+    if (toggle) {
+      unsetRepresentativeBadge(user!.id, badge.id);
+    } else {
+      setRepresentativeBadge(user!.id, badge.id);
+    }
+    setToggle((toggle) => !toggle);
+  };
+
   return (
-    <Wrapper>
+    <Wrapper style={{ cursor: cursor ? 'pointer' : 'auto' }} onClick={onToggle}>
       <Image
         src={iconUrl}
         alt="Badge Image"
         style={{
           borderRadius: '50%',
           display: 'block',
-          boxShadow: '1px 5px 10px rgba(0,0,0,0.5)',
         }}
         width={100}
         height={100}
       />
       <div className="title">{name}</div>
+      <CheckIcon toggle={toggle} cursor={cursor} />
     </Wrapper>
   );
 };
 
 export default BadgeItem;
+
+const CheckIcon = styled(AiOutlineCheckCircle)<{
+  cursor: boolean;
+  toggle: boolean;
+}>`
+  display: ${({ cursor, toggle }) =>
+    cursor ? (toggle ? 'block' : 'none') : 'none'};
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: ${COLORS.orange};
+  font-size: 3rem;
+`;
