72958a9b4e0c89fa77421ac7534d529b5c222db5
Eunbi Kang <jenabill@naver.com>
2022-08-20 15:08:18 +0900

    fix index page 충돌 해결

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,12 @@ import { useRecoilState } from 'recoil';
 import LoginForm from '../components/login/LoginForm';
 import sessionState from '../store/session';
 import { getSession } from '../apis/auth';
-import { supabase } from '../utils/supabase';
 
 const Home: NextPage = () => {
   const [session, setSession] = useRecoilState(sessionState);
   useEffect(() => {
-<<<<<<< HEAD
-    const data = getSession();
-    setSession(data);
-    supabase.auth.onAuthStateChange((_event, session) => {
-      console.log(_event);
-      setSession(session);
-    });
-  }, []);
-=======
     setSession(getSession());
   }, [setSession]);
->>>>>>> b0529d99862dd922b29dab835de2d1a93cb13a0d
   return <>{!session ? <LoginForm /> : 'Hello, World!'}</>;
 };
 
